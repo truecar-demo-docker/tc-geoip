7ecf1f12ac6a83a802610b1b08b3286433df373d
Regis Wilson <rwilson@truecar.com>
2018-08-01 20:30:47 +0000

    Add healthcheck and regexp paths with optional trailing slash

--- server.rb
+++ server.rb
@@ -8,6 +8,11 @@ before do
   content_type 'application/json'
 end
 
-get '/api' do
+get %r>/api/?> do
   @db.lookup(params[:ip]).to_hash.to_json
+end
+
+get %r>/internal/health/?> do
+  content_type 'text/plain'
+  'IMOK'
 end
