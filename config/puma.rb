f95726e3b43cc2ad16bbd8f04d3eba606f778f7c
Regis Wilson <rwilson@truecar.com>
2018-08-10 22:27:39 +0000

    Fix logic for min/max thread options

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,27 @@
 # Puma config
 DEFAULT_THREADS = 4
 DEFAULT_WORKERS = 1
-max_threads = [
-  ENV.fetch('PUMA_THREADS_MAX') { DEFAULT_THREADS }.to_i,
-  ENV.fetch('PUMA_THREADS') { DEFAULT_THREADS }.to_i,
-  ENV.fetch('PUMA_THREADS_MIN') { DEFAULT_THREADS }.to_i,
-].max
-min_threads = [
-  ENV.fetch('PUMA_THREADS_MAX') { DEFAULT_THREADS }.to_i,
-  ENV.fetch('PUMA_THREADS') { DEFAULT_THREADS }.to_i,
-  ENV.fetch('PUMA_THREADS_MIN') { DEFAULT_THREADS }.to_i,
-].min
+MAX_THREADS = 16
+MAX_WORKERS = 32
 
-workers ENV.fetch('PUMA_WORKERS') { DEFAULT_WORKERS }.to_i
+max_threads = [
+  ENV.fetch('PUMA_THREADS_MAX') { 0 }.to_i,
+  ENV.fetch('PUMA_THREADS') { 0 }.to_i,
+  ENV.fetch('PUMA_THREADS_MIN') { 0 }.to_i,
+].max
+max_threads = max_threads.between?(1, MAX_THREADS) ? max_threads : DEFAULT_THREADS
+
+min_threads = [
+  ENV.fetch('PUMA_THREADS_MAX') { MAX_THREADS + 1 }.to_i,
+  ENV.fetch('PUMA_THREADS') { MAX_THREADS + 1 }.to_i,
+  ENV.fetch('PUMA_THREADS_MIN') { 0 }.to_i,
+].min
+min_threads = min_threads.between?(1, MAX_THREADS) ? min_threads : DEFAULT_THREADS
+
+puma_workers = ENV.fetch('PUMA_WORKERS') { DEFAULT_WORKERS }.to_i
+puma_workers = puma_workers.between?(1, MAX_WORKERS) ? puma_workers : DEFAULT_WORKERS
+
+workers puma_workers
 threads min_threads, max_threads
 
 preload_app!
