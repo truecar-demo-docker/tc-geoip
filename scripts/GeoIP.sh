c479cf689a6dda7e53725ae11707fa8057dcc8f0
ylim <ylim@truecar.com>
2020-06-02 16:53:52 -0700

    attempt

--- scripts/GeoIP.sh
+++ scripts/GeoIP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-AWS_DEFAULT_REGION=us-west-2 aws ssm get-parameter --name /common/GeoIP.conf --with-decryption --query 'Parameter.Value' > /usr/local/etc/GeoIP.conf
+AWS_DEFAULT_REGION=us-west-2 aws ssm get-parameter --name /build/tc-geoip/LICENSE --with-decryption --query 'Parameter.Value' > /usr/local/etc/GeoIP.conf
