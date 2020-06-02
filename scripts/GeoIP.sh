20f0b59bc14f8452210c362daa344b365c8a5e05
C. Regis Wilson <rwilson@truecar.com>
2020-06-02 13:51:21 -0700

    Add region

--- scripts/GeoIP.sh
+++ scripts/GeoIP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws ssm get-parameter --name /common/GeoIP.conf --with-decryption --query 'Parameter.Value' > /usr/local/etc/GeoIP.conf
+AWS_DEFAULT_REGION=us-west-2 aws ssm get-parameter --name /common/GeoIP.conf --with-decryption --query 'Parameter.Value' > /usr/local/etc/GeoIP.conf
