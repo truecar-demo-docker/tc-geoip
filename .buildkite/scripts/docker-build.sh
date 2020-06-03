a48e5063c9ed8e9b61cea82cc5ed2afe94d6bba5
ylim <ylim@truecar.com>
2020-06-02 17:27:55 -0700

    attempt

--- .buildkite/scripts/docker-build.sh
+++ .buildkite/scripts/docker-build.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-export DOCKER_BUILD_OPTIONS="--build-arg LICENSE=${LICENSE}"
+export DOCKER_BUILD_OPTIONS="--build-arg LICENSE_KEY=${LICENSE_KEY} --build-arg LICENSE_USERID=${LICENSE_USERID}"
 
 curl -fsSL https://raw.git.corp.tc/infra/buildkite-resources/master/docker-build.sh | bash
